4ecddcaf6fce5e533b3ba00b7a2378a1a7a7dc5f
Diany <diany9012@yahoo.es>
2022-05-24 10:17:37 -0500

    Code clean up

---
+++ LACRM_employees.php
@@ -0,0 +1,25 @@
+<?php
+$myfile = fopen("employees.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("employees.txt"));
+
+$employeesArray = $myfile;
+
+echo count($employeesArray);
+
+
+
+
+
+
+
+
+
+fclose($myfile);
+
+
+
+// - List the LACRM employees in alphabetical order
+//     - Make an array of LACRM employees that isn’t sorted alphabetically by default (you don’t have to include everyone if you don’t want)
+//     - Use PHP to sort the list alphabetically (you’ll have to look up how to do this)
+//     - Loop through the array and echo each person’s name (use line breaks and css to make it pleasant to look at)
+//     - Make a version that does it in reverse alphabetical order
--- chessboard.php
+++ chessboard.php
@@ -1,35 +1,34 @@
 <?php 
 
-echo "<html>";                                                                                                      
-    echo "<head>";
-        echo "<title>My Chess Board</title>";                                                          
-    echo "<link href='ChessBoard.css' rel='stylesheet'>";
-    echo "</head>";
+echo "<html>                                                                                                      
+        <head>
+            <title>My Chess Board</title>                                                         
+            <link href='ChessBoard.css' rel='stylesheet'>
+        </head>
+        
+        <body>";                                                                                          
+            $white = "white";
+            $black = "black";
 
-    echo "<body>";                                                                                          
-      $white = "white";
-      $black = "black";
+            for($i = 1; $i < 64/2 + 1; $i++)
+            {
+                if($i == 1 or $i % 4 == 1){
+                    echo "<div class='board'>";
+                }     
 
-     for($i = 1; $i < 64/2 + 1; $i++)
-     {
-        if($i == 1 or $i % 4 == 1){
-            echo "<div class='board'>";
-        }     
+                echo "<h1 class='Square' style='background-color: $white;'></h1>
+                    <h1 class='Square' style='background-color: $black;'></h1>"; 
 
-        echo "<h1 class='Square' style='background-color: $white;'></h1>";
-        echo "<h1 class='Square' style='background-color: $black;'></h1>"; 
+                if($i != 0 and $i % 4 == 0){
+                    echo "</div>";       
+                    $temp = $white;
+                    $white = $black;
+                    $black = $temp; 
+                }       
+            }    
 
-        if($i != 0 and $i % 4 == 0){
-            echo "</div>";       
-            $temp = $white;
-            $white = $black;
-            $black = $temp; 
-        }         
-                  
-    }    
-
-    echo "</body>" ;  
-    echo "</html>"; 
+    echo "</body>   
+    </html>"; 
     
     
 
