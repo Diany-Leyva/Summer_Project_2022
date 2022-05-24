23a12a480ffdc3d48da70aa218767977fac877df
Diany <diany9012@yahoo.es>
2022-05-24 12:04:26 -0500

    Chess

--- LACRM_employees.php
+++ LACRM_employees.php
@@ -1,10 +1,6 @@
 <?php
-$myfile = fopen("employees.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("employees.txt"));
 
-$employeesArray = $myfile;
 
-echo count($employeesArray);
 
 
 
@@ -23,3 +19,27 @@ fclose($myfile);
 //     - Use PHP to sort the list alphabetically (you’ll have to look up how to do this)
 //     - Loop through the array and echo each person’s name (use line breaks and css to make it pleasant to look at)
 //     - Make a version that does it in reverse alphabetical order
+
+
+
+<?php
+// Get a file into an array.  In this example we'll go through HTTP to get
+// the HTML source of a URL.
+$lines = file('http://www.example.com/');
+
+// Loop through our array, show HTML source as HTML source; and line numbers too.
+foreach ($lines as $line_num => $line) {
+    echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
+}
+
+// Using the optional flags parameter
+$trimmed = file('somefile.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+?>
+
+
+
+
+$myfile = fopen("employees.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("employees.txt"));
+$employees = array($myfile);
+echo count($employees);
--- chessboard.php
+++ chessboard.php
@@ -8,7 +8,7 @@ echo "<html>
         
         <body>";                                                                                          
             $white = "white";
-            $black = "black";
+            $black = "#292929";
 
             for($i = 1; $i < 64/2 + 1; $i++)
             {
@@ -16,8 +16,93 @@ echo "<html>
                     echo "<div class='board'>";
                 }     
 
-                echo "<h1 class='Square' style='background-color: $white;'></h1>
-                    <h1 class='Square' style='background-color: $black;'></h1>"; 
+                echo "<h1 class='Square' style='background-color: $white;'>";
+                
+                if($i == 1){
+                    echo "<img src='images/white_rook.svg' alt='white_rook' class='image'>";
+                }
+
+                else if($i == 2){
+                    echo "<img src='images/white_bishop.svg' alt='white_bishop' class='image'>";
+                }
+
+                else if($i == 3){
+                    echo "<img src='images/white_queen.svg' alt='white_queen' class='image'>";
+                }
+
+                else if($i == 4){
+                    echo "<img src='images/white_knight.svg' alt='white_knight' class='image'>";
+                }
+
+                else if($i >= 5 && $i <=8){
+                    echo "<img src='images/white_pawn.svg' alt='white_pawn' class='image'>";
+                }
+
+                else if($i >= 25 && $i <= 28){
+                    echo "<img src='images/black_pawn.svg' alt='black_pawn' class='image'>";
+                }
+
+                else if($i == 29){
+                    echo "<img src='images/Black_Rook.svg' alt='Black Rook' class='image'>";
+                }
+
+                else if($i == 30){
+                    echo "<img src='images/black_bishop.svg' alt='black_bishop' class='image'>";
+                }
+
+                else if($i == 31){
+                    echo "<img src='images/black_queen.svg' alt='black_queen' class='image'>";
+                }
+
+                else if($i == 32){
+                    echo "<img src='images/black_knight.svg' alt='black_queen' class='image'>";
+                }
+
+                echo "</h1>";
+                
+                echo"<h1 class='Square' style='background-color: $black;'>"; 
+
+                if($i == 1){
+                    echo "<img src='images/white_knight.svg' alt='images/knight' class='image'>";
+                }
+
+                else if($i == 2){
+                    echo "<img src='images/white_king.svg' alt='white_king' class='image'>";
+                }
+
+                else if($i == 3){
+                    echo "<img src='images/white_bishop.svg' alt='Black Rook' class='image'>";
+                }
+
+                else if($i == 4){
+                    echo "<img src='images/white_rook.svg' alt='Black Rook' class='image'>";
+                }
+
+                else if($i >= 5 && $i <=8){
+                    echo "<img src='images/white_pawn.svg' alt='white_pawn' class='image'>";
+                }
+
+                else if($i >= 25 && $i <= 28){
+                    echo "<img src='images/black_pawn.svg' alt='black_pawn' class='image'>";
+                }
+
+                else if($i == 29){
+                    echo "<img src='images/black_knight.svg' alt='black_knight' class='image'>";
+                }
+
+                else if($i == 30){
+                    echo "<img src='images/black_king.svg' alt='black_king' class='image'>";
+                }
+
+                else if($i == 31){
+                    echo "<img src='images/black_bishop.svg' alt='black_queen' class='image'>";
+                }
+
+                else if($i == 32){
+                    echo "<img src='images/black_rook.svg' alt='black_queen' class='image'>";
+                }
+
+                echo "</h1>";
 
                 if($i != 0 and $i % 4 == 0){
                     echo "</div>";       
@@ -25,50 +110,11 @@ echo "<html>
                     $white = $black;
                     $black = $temp; 
                 }       
-            }    
-
-    echo "</body>   
+            }            
+            
+echo    "</body>   
     </html>"; 
     
     
-
-
-
-// php and html attempt
-
-<html>                                                                                                      
-<head>
-    <title>My Chess Board</title>                                                             
-    <link href="ChessBoard.css" rel="stylesheet">
-</head>
-
-<body>                                                                                                  
-
-  <?php $white = "#ffffff";
-        $black = "#000000";?>
-
-  <?php for($i = 1; $i < 64/2 + 1; $i++) :?>
-    <?php if($i == 1 or $i % 4 == 1) :?>
-    <div class="board">
-
-    <h1 class="Square" style="background-color: <?php $white ?>;"></h1>
-    <h1 class="Square" style="background-color: <?php $black ?>;"></h1>  
- 
-    <?php if($i != 0 and $i % 4 == 0) :?>
-    </div>    
-    $temp = $white;
-    $white = $black;
-    $black = $temp;?>
-  <?php endfor; ?>                    
-    
-</body>    
-</html>
-
-
-
-
-
-
-
 
 
