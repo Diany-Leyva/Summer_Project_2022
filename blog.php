dba001998e6dc36cf204ea7b1df4e9bd8ca82582
Diany <diany9012@yahoo.es>
2022-05-25 14:33:58 -0500

    1

--- blog.php
+++ blog.php
@@ -4,17 +4,39 @@
         <link href="style.css" rel="stylesheet">                
     </head>
     <body>                
-        <header></header>  
-        <div>        
-            <h1>Dance</h1> 
-            <h1>Motherhood</h1>
-            <h1>Chess</h1>
-            <h1>Minimalism</h1>
-            <h1>Healthy Lifestyle</h1>
-           
+        <header><img class= 'image' src="blog.jpg" alt="blog"></header>  
+        <div>  
+        <ul class="a">
+            <h2>Here is my <span style= "color: #AF766E; ">Blog:</span><br/></h2>
+            <div class= 'list1'>                   
+            <li><a href="dance.php" class="buttons2"> Dance</a></li>
+            <li>Motherhood</li>
+            <li>Chess</li>
+            <li>Minimalism</li>
+            <li>Healthy Lifestyle</li>
+            </div> 
+            
+  
+
+
+
+
+
+
+<!-- 
+            <li><a href="dance.php" class="buttons2"> Dance</a></li>  
+            <a href="dance.php" class="buttons2"> Motherhood</a></br>     
+            <a href="dance.php" class="buttons2"> Chess</a></br>    
+            <a href="dance.php" class="buttons2"> Minimalism</a></br>        
+            <a href="dance.php" class="buttons2"> Healthy Lifestyle</a></br>     -->
+        </u>      
+            
+              
         </div>        
              
        
     </body>  
 
 </html>
+
+
