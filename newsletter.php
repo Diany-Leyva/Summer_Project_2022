ea82ef283e126cf15feefc1ecb42c45ff04612f8
Diany <diany9012@yahoo.es>
2022-05-27 16:52:36 -0500

    1

--- newsletter.php
+++ newsletter.php
@@ -27,29 +27,5 @@ echoHeader('Newsletter', 'image', 'a');
 
 // var_dump($errors);
 debugOutput($errors);
-
-
-// $_REQUEST[] = "hkjchkjchk";
-
-//Here we are storing in the database what is written in the URL
-// dbQuery("
-//     INSERT INTO newsletter_subscriber(Name, Email)
-//     VALUES ('$_REQUEST[Name]', '$_REQUEST[Email]' )
-// ");
-
-//variable that always exits it is how your submit content to the page, which content to submit, what the data is
-// debugOutput($_REQUEST);
-
-
-// Pretty much every form will be a post, if I put the get the information will be seen in the url
-//action is to submit the information to a different page  
-
-
 createNewsletterForm();
-
-
-
-
-
-
 echoFooter();
