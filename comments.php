aaf99de5d8357b7f7f9ebabced519af14ffe19c2
Diany <diany9012@yahoo.es>
2022-06-01 16:59:54 -0500

    1

--- comments.php
+++ comments.php
@@ -15,6 +15,13 @@ $errors = [];
 <p class = 'areacomment'>Comment</p>
 <textarea ></textarea>
 
+<form action='' method = 'post'>                                       
+Name: <input type= 'text' name='Name' />
+<br> </br>
+Email: <input type= 'text' name='Email' />
+        <br> </br>
+        <input type= 'submit' name='NewsletterFormSubmit'/>
+        </form> 
 
 
 
