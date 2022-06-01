8c5b99a96b4b836396a493ff4dc7b5060814e331
Diany <diany9012@yahoo.es>
2022-06-01 13:41:40 -0500

    1

--- view_posts.php
+++ view_posts.php
@@ -8,7 +8,7 @@ echoBackgroundImageHeader('pagesHeader');
 echo"<h2>";
     echoHeadingTwoColors($post['Heading']);
 echo"</h2>
-    <div class=squareBackground2>
+    <div class=squareBackground>
         <p>".$post['Content']. "</p>
     </div>";            
 echoFooter();
