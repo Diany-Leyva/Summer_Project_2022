5bdb617991e16458168ce3ed513c1ac0d201d1ef
Diany <diany9012@yahoo.es>
2022-06-01 13:31:18 -0500

    Borders added

--- post.php
+++ post.php
@@ -12,14 +12,16 @@ echo"<h2>";
        echoHeadingTwoColors($topic['Heading']);
 echo"</h2>
    
+<div class=squareBackground>
      <div class= 'list1'> 
           <ul class='a'>";  
               foreach($allPostsbyTopic as $postByTopic) {
                     passVariableThroughLink('view_posts.php', $postByTopic['Post_Id'], 'buttons2', $postByTopic['Tittle']);               
              }
                                        
-echo"     </u>         
-     </div>";
+echo"     </u>
+     </div>         
+</div>";
         
 echoFooter();
     
