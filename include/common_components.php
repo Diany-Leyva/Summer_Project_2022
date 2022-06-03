6d2ade2d6756d84dfb29f147de5dbec7a4fedf60
Diany <diany9012@yahoo.es>
2022-06-03 17:56:59 -0500

    Comments_Form

--- include/common_components.php
+++ include/common_components.php
@@ -71,36 +71,39 @@ echo"   <form method = 'post' action=''>
 
 function createCommentsForm(){
 echo"
+    <div class= commentsForm>
         <h1 class ='reply'>
     ";     
             echoHeadingTwoColors('Leave a reply');
 echo"  
         </h1>
-        <h3 class='reply'>Your email address will not be published. Required fields are marked *<br>   </h3>
-        <form method = 'post' action=''> 
-            <p class ='areacomment'>Comment</p>
-            <textarea cols='45' rows='8' maxlength='65525' name='Content'></textarea>                  
-            <ul> 
-                <li>
-                    <label for='name'>Name *</label>                                  
-                    <input type='text' id = 'name' name='Name'>
-                </li>
-                <br> </br>  
-                <li>
-                    <label for='mail'>Email *</label>
-                    <input type='text' id='mail' name='Email'>       
-                </li> 
-                <br> </br>
-                <li> 
-                    <label for='website'>Website</label>
-                    <input type='text' id='website' name='Website'>                         
-                </li> 
-                <br></br>
-                <li>
-                    <input  class = 'buttons text reply' type= 'submit' name='CommentsFormPostComment' value='Post Comment'>
-                </li>
-            </ul>          
-        </form>  
+       
+            <h3 class='reply'>Your email address will not be published. Required fields are marked *<br>   </h3>
+            <form method = 'post' action=''> 
+                <p class ='areacomment'>Comment</p>
+                <textarea cols='45' rows='8' maxlength='65525' name='Content'></textarea>                  
+                <ul> 
+                    <li>
+                        <label for='name'>Name *</label>                                  
+                        <input type='text' id = 'name' name='Name'>
+                    </li>
+                    <br> </br>  
+                    <li>
+                        <label for='mail'>Email *</label>
+                        <input type='text' id='mail' name='Email'>       
+                    </li> 
+                    <br> </br>
+                    <li> 
+                        <label for='website'>Website</label>
+                        <input type='text' id='website' name='Website'>                         
+                    </li> 
+                    <br></br>
+                    <li>
+                        <input  class = 'buttons text reply' type= 'submit' name='CommentsFormPostComment' value='Post Comment'>
+                    </li>
+                </ul>          
+            </form>  
+        </div>
     ";
 }
 
