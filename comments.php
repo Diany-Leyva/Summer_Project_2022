2f25476c3937360726ec43a84df522948acc4527
Diany <diany9012@yahoo.es>
2022-06-08 09:38:36 -0500

    1

--- comments.php
+++ comments.php
@@ -15,6 +15,7 @@ if(isset($_REQUEST['CommentsFormPostComment'])){
     if(sizeof($errors) == 0){
         updateUser_TableDB();                                           
         $userId = getNewUserId($_REQUEST['Name'], $_REQUEST['Email']);
+        debugOutput($userId);
         updateComments_TableDB($userId);                                  
         
         header("location:?");                                           
--- include/database_functions.php
+++ include/database_functions.php
@@ -10,24 +10,24 @@ function updateNewsletter_Subscriber_TableDB(){
 function updateComments_TableDB($userId){
     dbQuery("
     INSERT INTO comments(Content, User_Id, Post_Id)
-    VALUES ('$_REQUEST[Content]', $userId, '$_REQUEST[Post_Id]')
+    VALUES ('$_REQUEST[Content]', '$userId', '$_REQUEST[Post_Id]')
 ");
 }
 
 function updateUser_TableDB(){
     dbQuery("
     INSERT INTO users(Name, Email, Website)
-    VALUES ('$_REQUEST[Name]', '$_REQUEST[Email]', '$_REQUEST[Website]' )
+    VALUES ('$_REQUEST[Name]', '$_REQUEST[Email]', '$_REQUEST[Website]')
 ");
 }
 
 function getNewUserId($name, $email){
-    $id = dbQuery("
+    $userID = dbQuery("
         SELECT User_Id 
         FROM users
-        WHERE Name = $name 
-        AND Email = $email  
+        WHERE Name = '$name' 
+        AND Email = '$email'  
     ")->fetch();
 
-    return $id;
+    return $userID;
 }
