67f48f0aa70382d95e7f1af3c13f15ea44b90682
Diany <diany9012@yahoo.es>
2022-06-09 09:50:23 -0500

    Jog 1 (Waiting review)

---
+++ include/database_functions.php
@@ -0,0 +1,32 @@
+<?php
+
+function getAllStudents(){
+    return dbQuery("
+            SELECT *
+            FROM students        
+            ")->fetchAll();   
+}
+
+function getAllClasses(){
+    return dbQuery("
+            SELECT *
+            FROM classes;   
+            ")->fetchAll();  
+}
+
+function getAllClassesByStudent($studentId){
+    return dbQuery("
+        SELECT *
+        FROM classes
+        WHERE Student_id = $studentId  
+        ORDER BY Start_Date
+        ")->fetchAll();  
+}
+
+function getStudent($studentId){
+    return dbQuery("
+        SELECT *
+        FROM students
+        WHERE Student_Id = $studentId;   
+        ")->fetch();  
+}
