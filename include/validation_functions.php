03e5ca9438f66115f3d11b9f26128b2d7ab09678
Diany <diany9012@yahoo.es>
2022-06-14 12:00:32 -0500

    Update validation_functions.php

--- include/validation_functions.php
+++ include/validation_functions.php
@@ -22,3 +22,49 @@ function isEmpty($dataReturned){
 //     return  false;
 // }
 
+
+
+// function validateStudents(){
+//     //I will use this function to validate data of students 
+// }
+
+// function validateClasses(){
+//     //I will use this function to validate data of students 
+// }
+
+// function test_input($data) {
+//     $data = trim($data);
+//     $data = stripslashes($data);
+//     $data = htmlspecialchars($data);
+//     return $data;
+// }
+
+// function validateContent(&$errors){
+//     if(!isset($_REQUEST['Content']) || $_REQUEST['Content'] == ''){
+//         $errors['Content'] = 'Required';
+//     }
+// }
+
+// function validateName(&$errors){
+
+//     if(!isset($_REQUEST['Name']) || $_REQUEST['Name'] == ''){
+//         $errors['Name'] = 'Required';
+//     }
+
+//     $name = test_input($_REQUEST['Name']);
+
+//     if (!preg_match('/^[a-zA-Z- ]*$/',$name)) {
+//         $errors['Name'] = "Only letters and white space allowed";
+//     }
+// }
+
+// function validateEmail(&$errors){
+
+//     if(!isset($_REQUEST['Email']) || $_REQUEST['Email'] == ''){
+//         $errors['Email'] = 'Required';
+//     }   
+
+//     else if (!filter_var(test_input($_REQUEST["Email"]), FILTER_VALIDATE_EMAIL)) {
+//         $errors['Email'] = "Invalid email format";
+//     }
+// }
